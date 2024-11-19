a3acb493a5c93047cb1070a02ab59c609148364f
SiddhantMohekar <133594916+SiddhantMohekar@users.noreply.github.com>
2024-11-19 19:42:40 +0530

    t

--- src/main/java/com/activitymains/activitymains/Security/Config/WebSecurityConfig.java
+++ src/main/java/com/activitymains/activitymains/Security/Config/WebSecurityConfig.java
@@ -8,14 +8,18 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.context.DelegatingSecurityContextRepository;
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.security.web.context.RequestAttributeSecurityContextRepository;
 import org.springframework.security.web.context.SecurityContextRepository;
 
+import com.activitymains.activitymains.Security.JwtAuthenticationEntryPoint;
+import com.activitymains.activitymains.Security.JwtAuthenticationFilter;
 import com.activitymains.activitymains.Service.ImageService;
 
 @Configuration
@@ -26,6 +30,11 @@ public class WebSecurityConfig {
 
     @Autowired
     UserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtAuthenticationEntryPoint point;
+    @Autowired
+    private JwtAuthenticationFilter filter;
 
     @Bean
     SecurityContextRepository securityContextRepository(){
@@ -48,9 +57,20 @@ public class WebSecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
     
-        http.authorizeHttpRequests(authorize->authorize.requestMatchers(HttpMethod.GET,"/Events","/index","/Profile","/login","/Register","/user-name").hasAnyRole("USER","ADMIN")
-        .requestMatchers("/","/login","/index","/user-name").permitAll()
-        );
+        http.authorizeHttpRequests(authorize->{
+            try {
+                authorize.requestMatchers(HttpMethod.GET,"/Events","/index","/Profile","/login","/Register","/user-name").hasAnyRole("USER","ADMIN")
+                .requestMatchers("/","/login","/index","/user-name").permitAll().
+                anyRequest().authenticated()
+                .and().exceptionHandling(ex->ex.authenticationEntryPoint(point))
+                .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        ).csrf(csrf->csrf.disable()).cors(cors->cors.disable());
+
+        http.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 }
